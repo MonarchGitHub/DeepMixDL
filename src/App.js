2171ffc6bb57779999fd368ef3529ca8c35e4026
Monarch <monarchk7@gmail.com>
2022-05-15 00:27:02 +0530

    added starter files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main_title">
+      <div className="logo">
+        <h3>Logo Goes Here</h3>
+      </div>
+      <h3>DeepMix: Music Generation With DeepLearning</h3>
     </div>
   );
 }
